d92f1a5839f444e69aaa63bcec6a055fb49e7f04
ABDUL MAJID K <majidmaji3410@gmail.com>
2025-06-20 11:40:50 +0400

    Configure capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  "appId": "com.vynox.restaurantpos",
+  "appName": "Restaurant POS",
+  "webDir": "dist"
+};
+
+export default config;
